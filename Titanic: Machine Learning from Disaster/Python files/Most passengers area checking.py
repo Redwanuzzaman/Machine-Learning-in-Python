8d80fc25e3330a3afcf8e1ab30b698e93f0090b8
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 00:46:54 +0600

    Create Most passengers area checking.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Most passengers area checking.py
@@ -0,0 +1,8 @@
+# From which place most of the passengers get in the ship
+
+Pclass1 = train[train['Pclass']==1]['Embarked'].value_counts()
+Pclass2 = train[train['Pclass']==2]['Embarked'].value_counts()
+Pclass3 = train[train['Pclass']==3]['Embarked'].value_counts()
+df = pd.DataFrame([Pclass1, Pclass2, Pclass3])
+df.index = ['1st class','2nd class', '3rd class']
+df.plot(kind='bar',stacked=True, figsize=(10,5))
