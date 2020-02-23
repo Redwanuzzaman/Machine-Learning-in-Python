90dfcc963faeb3119fb0a1e80fd4f738587ec4dc
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 00:30:01 +0600

    Create Survival Rate in age variable.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Survival Rate in age variable.py
@@ -0,0 +1,15 @@
+# Survival Rate in age variable
+
+facet = sns.FacetGrid(train, hue="Survived",aspect=4)
+facet.map(sns.kdeplot,'Age',shade= True)
+facet.set(xlim=(0, train['Age'].max()))
+facet.add_legend()
+
+plt.show()
+
+facet = sns.FacetGrid(train, hue="Survived",aspect=4)
+facet.map(sns.kdeplot,'Age',shade= True)
+facet.set(xlim=(0, train['Age'].max()))
+facet.add_legend()
+
+plt.xlim(28, 40)
