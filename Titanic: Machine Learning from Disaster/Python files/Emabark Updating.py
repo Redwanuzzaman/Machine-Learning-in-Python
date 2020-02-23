98bf9815f14ffb4de05412d593c58b8491ee9e7e
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 00:49:16 +0600

    Update Emabark Updating.py

--- Titanic: Machine Learning from Disaster/Python files/Emabark Updating.py
+++ Titanic: Machine Learning from Disaster/Python files/Emabark Updating.py
@@ -1,3 +1,7 @@
-#filling up most of the places with Southampton symboling "S"
+#filling up most of the places with Southampton symboling "S" and mapping Embarked with numerical values
 for dataset in train_test_data:
     dataset['Embarked'] = dataset['Embarked'].fillna('S')
+    
+embarked_mapping = {"S": 0, "C": 1, "Q": 2}
+for dataset in train_test_data:
+    dataset['Embarked'] = dataset['Embarked'].map(embarked_mapping)
