ad5d9c9e539f2785ccf1a21dacf4d5b21efb4455
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 01:07:25 +0600

    Create Family Size.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Family Size.py
@@ -0,0 +1,9 @@
+# Family Size
+train["FamilySize"] = train["SibSp"] + train["Parch"] + 1
+test["FamilySize"] = test["SibSp"] + test["Parch"] + 1
+
+facet = sns.FacetGrid(train, hue="Survived",aspect=4)
+facet.map(sns.kdeplot,'FamilySize',shade= True)
+facet.set(xlim=(0, train['FamilySize'].max()))
+facet.add_legend()
+plt.xlim(0)
