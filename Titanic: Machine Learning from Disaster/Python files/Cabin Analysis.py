631e919b5fdb7c08f1c0d728db697fbd15254c8b
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 01:04:20 +0600

    Create Cabin Analysis.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Cabin Analysis.py
@@ -0,0 +1,10 @@
+# Cabin Analysis
+for dataset in train_test_data:
+    dataset['Cabin'] = dataset['Cabin'].str[:1]
+    
+Pclass1 = train[train['Pclass']==1]['Cabin'].value_counts()
+Pclass2 = train[train['Pclass']==2]['Cabin'].value_counts()
+Pclass3 = train[train['Pclass']==3]['Cabin'].value_counts()
+df = pd.DataFrame([Pclass1, Pclass2, Pclass3])
+df.index = ['1st class','2nd class', '3rd class']
+df.plot(kind='bar',stacked=True, figsize=(10,5))
