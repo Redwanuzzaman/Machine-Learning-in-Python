5d16c8c796db81eac7fadd8bd5c0e2b0b18dea9c
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 01:00:07 +0600

    Create Mapping Fare.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Mapping Fare.py
@@ -0,0 +1,6 @@
+# Mapping Fare
+for dataset in train_test_data:
+    dataset.loc[ dataset['Fare'] <= 17, 'Fare'] = 0,
+    dataset.loc[(dataset['Fare'] > 17) & (dataset['Fare'] <= 30), 'Fare'] = 1,
+    dataset.loc[(dataset['Fare'] > 30) & (dataset['Fare'] <= 100), 'Fare'] = 2,
+    dataset.loc[ dataset['Fare'] > 100, 'Fare'] = 3
