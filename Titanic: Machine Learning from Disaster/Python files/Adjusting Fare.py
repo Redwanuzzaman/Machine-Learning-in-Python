be03010e6d4a6cbd47876c8828ff541ab3bf768c
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 00:53:32 +0600

    Create Adjusting Fare.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Adjusting Fare.py
@@ -0,0 +1,5 @@
+# fill missing Fare with median fare for each Pclass
+
+train["Fare"].fillna(train.groupby("Pclass")["Fare"].transform("median"), inplace=True)
+test["Fare"].fillna(test.groupby("Pclass")["Fare"].transform("median"), inplace=True)
+train.head(5)
