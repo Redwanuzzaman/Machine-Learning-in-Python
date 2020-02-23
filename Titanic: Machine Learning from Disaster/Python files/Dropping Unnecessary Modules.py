74b463a2f86e19b2f4272146a1e7b89e28ff09d0
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 01:14:39 +0600

    Create Dropping Unnecessary Modules.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Dropping Unnecessary Modules.py
@@ -0,0 +1,10 @@
+# Dropping Unnecessary Variables
+features_drop = ['Ticket', 'SibSp', 'Parch']
+train = train.drop(features_drop, axis=1)
+test = test.drop(features_drop, axis=1)
+train = train.drop(['PassengerId'], axis=1)
+
+train_data = train.drop('Survived', axis=1)
+target = train['Survived']
+
+train_data.shape, target.shape
