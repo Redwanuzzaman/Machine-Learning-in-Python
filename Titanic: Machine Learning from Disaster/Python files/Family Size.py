cf9d859d65609f9391bc597f16efbabdd31159be
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 01:08:36 +0600

    Update Family Size.py

--- Titanic: Machine Learning from Disaster/Python files/Family Size.py
+++ Titanic: Machine Learning from Disaster/Python files/Family Size.py
@@ -7,3 +7,8 @@ facet.map(sns.kdeplot,'FamilySize',shade= True)
 facet.set(xlim=(0, train['FamilySize'].max()))
 facet.add_legend()
 plt.xlim(0)
+
+# Mapping
+family_mapping = {1: 0, 2: 0.4, 3: 0.8, 4: 1.2, 5: 1.6, 6: 2, 7: 2.4, 8: 2.8, 9: 3.2, 10: 3.6, 11: 4}
+for dataset in train_test_data:
+    dataset['FamilySize'] = dataset['FamilySize'].map(family_mapping)
