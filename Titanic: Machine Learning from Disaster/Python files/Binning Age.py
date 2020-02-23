38bbbfbc640e7a035617d65b030687abe2f65c82
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-24 00:27:29 +0600

    Create Binning Age.py

---
+++ Titanic: Machine Learning from Disaster/Python files/Binning Age.py
@@ -0,0 +1,8 @@
+# Binning age variable in 5 categories
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0,
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 26), 'Age'] = 1,
+    dataset.loc[(dataset['Age'] > 26) & (dataset['Age'] <= 36), 'Age'] = 2,
+    dataset.loc[(dataset['Age'] > 36) & (dataset['Age'] <= 62), 'Age'] = 3,
+    dataset.loc[ dataset['Age'] > 62, 'Age'] = 4
